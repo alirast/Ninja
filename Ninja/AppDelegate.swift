56e9c943cd3799e7809f8ae7625b1845ab4ca04b
alirast <xdabnormal@gmail.com>
2022-12-28 21:57:28 +0300

    Initial Commit

---
+++ Ninja/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Ninja
+//
+//  Created by n on 17.11.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
